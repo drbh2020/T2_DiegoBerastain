21bcaea35425a52b6a90fdf165f6d9482645cfe3
Diego Berastain <berastaindiego@gmail.com>
2025-12-13 21:55:39 -0500

    Initial Commit

---
+++ T2_DiegoBerastain/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  T2_DiegoBerastain
+//
+//  Created by user286645 on 12/13/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ T2_DiegoBerastainTests/T2_DiegoBerastainTests.swift
@@ -0,0 +1,17 @@
+//
+//  T2_DiegoBerastainTests.swift
+//  T2_DiegoBerastainTests
+//
+//  Created by user286645 on 12/13/25.
+//
+
+import Testing
+@testable import T2_DiegoBerastain
+
+struct T2_DiegoBerastainTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
